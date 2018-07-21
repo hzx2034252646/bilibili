5d036030d98b309697f3acde2a8ad9f80b8164e4
hzx2034252646 <2034252646@qq.com>
2018-07-21 17:39:15 +0800

    Initial commit

--- code/build/webpack.prod.conf.js
+++ code/build/webpack.prod.conf.js
@@ -23,7 +23,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       usePostCSS: true
     })
   },
-  devtool: config.build.productionSourceMap ? config.build.devtool : false,
+  // devtool: config.build.productionSourceMap ? config.build.devtool : false,
+  devtool: false,
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
